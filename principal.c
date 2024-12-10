94a3870380540dea1fb835f34da2c6443b085f23
diegoperesmarques@gmail.com <102305401+perescmexx@users.noreply.github.com>
2024-12-10 20:14:31 -0300

    Função padrão menu e cadastro produto

--- principal.c
+++ principal.c
@@ -1,54 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu_inicial_titulo();
-void menu_inicial_opcoes();
+void titulo_formatado();
+int menu_opecoes();
+void opcoes_inicio();
+void menu_cadastro_produto();
+void menu_produto();
 
 int main() {
-	menu_inicial_titulo();
-	menu_inicial_opcoes();
+	titulo_formatado("Bem vindo ao sistema de comércio!");
+	char *opcoes[] = {"1 - PRODUTO", "2 - FUNCIONARIOS"};
+	int opcao_escolhida;
+	opcao_escolhida = menu_opecoes(2, opcoes);
+	opcoes_inicio(opcao_escolhida);
 	
 	printf("\n");
 	return 0;
 }
 
-void menu_inicial_titulo () {
+void titulo_formatado (char mensagem[]) {
 	system("clear");
 	printf("###########################################");
 	printf("\n");
-	printf("Bem vindo ao sistema de comércio!");
+	printf("%s", mensagem);
 	printf("\n");
 	printf("###########################################");
 	printf("\n");
 }
 
+void opcoes_inicio(int opcao_escolhida) {
+	switch(opcao_escolhida) {
+	case 1:
+		menu_produto();
+		break; 
+	case 2: 
+		printf("Acessando cadastro de funcionário");
+		break;
+	}
+}
 
-void menu_inicial_opcoes() {
+
+int menu_opecoes(int maximo_opcoes, char *opcoes[]) {
 	int opcao_escolhida;
 	
 	printf("\n");
-	printf("1 - CADASTRO PRODUTOS \n");
-	printf("2 - CADASTRO FUNCIONARIOS \n");
+	for(int i = 0; i < maximo_opcoes; i++) {
+		printf("%s", opcoes[i]);
+		printf("\n");
+	}
+
 	printf("Digite o número da opção: ");
 	scanf("%d", &opcao_escolhida);
 	
-	while(opcao_escolhida < 1 || opcao_escolhida > 2) {
+	while(opcao_escolhida < 1 || opcao_escolhida > maximo_opcoes) {
 		system("clear");
 		printf("Opção incorreta, tente novamente");
+
 		printf("\n");
-		printf("1 - CADASTRO PRODUTOS \n");
-		printf("2 - CADASTRO FUNCIONARIOS \n");
+		for(int i = 0; i < maximo_opcoes; i++) {
+			printf("%s", opcoes[i]);
+			printf("\n");
+		}
+
 		printf("Digite o número da opção: ");
 		scanf("%d", &opcao_escolhida);
+
 	}
+
+	return opcao_escolhida;
 	
-	switch(opcao_escolhida) {
-		case 1:
-			printf("Acessando cadastro de produto");
-			break; 
-		case 2: 
-			printf("Acessando cadastro de funcionário");
-			break;
-	}
-	
+}
+
+
+void menu_produto() {
+	titulo_formatado("Menu de cadastro de produtos!");
+
+
 }
